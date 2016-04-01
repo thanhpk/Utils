3d5da97143b339fc0e19678285d7121081fdc061
Phạm Kiều Thanh <thanhpk@users.noreply.github.com>
2016-04-02 01:18:25 +0700

    random much more random

---
+++ js/superrandom.js
@@ -0,0 +1,31 @@
+function randomNumber(a,b)
+{
+	if(b == undefined) {
+		b = a - 1;
+		a = 0;
+	}
+	var delta = b - a + 1;
+	return Math.floor(Math.random()*delta) + a
+}
+
+var h = [];
+function randomSuper(a,b)
+{
+	if(h.length == 0)
+  {
+  	//init hash
+		var delta = b - a;
+		for(var i = 0 ; i < delta ; i++)
+  	{
+  		if(i==0) h[i] = randomNumber(0,20);
+   	 else h[i] = h[i-1] + randomNumber(0,20);
+  	}
+  }
+  var r = randomNumber(0, h[delta-1]);
+  
+  for(var i =0; i < delta; i++)
+  	if(h[i] >= r) return i + a;
+   return 'fuck'
+}
+
+alert(randomSuper(0,3));
