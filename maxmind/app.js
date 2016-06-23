6655447b1b7a93c865b2e658c81fe69e33cdd519
Pham Kieu Thanh <thanhpk@live.com>
2016-06-23 18:41:57 +0700

    done

--- maxmind/app.js
+++ maxmind/app.js
@@ -1,8 +1,6 @@
 var maxmind = require('maxmind');
 
-var cityLookup = maxmind.open('/path/to/GeoLite2-City.mmdb');
-var city = lookup.get('66.6.44.4');
+var cityLookup = maxmind.open('./GeoLite2-City.mmdb');
+var city = cityLookup.get('42.113.94.71');
 
-var orgLookup = maxmind.open('/path/to/GeoOrg.mmdb');
-var organization = orgLookup.get('66.6.44.4');
-console.log(organization);
+console.log(city);
