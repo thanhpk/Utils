6a559a90cd31808f96a17c91a3366fe0ea708235
Pham Kieu Thanh <thanhpk@live.com>
2016-06-23 18:45:03 +0700

    add cache

--- maxmind/app.js
+++ maxmind/app.js
@@ -1,6 +1,9 @@
 var maxmind = require('maxmind');
 
-var cityLookup = maxmind.open('./GeoLite2-City.mmdb');
+var cityLookup = maxmind.open('./GeoLite2-City.mmdb', {cache: {
+    max: 1000, // max items in cache
+    maxAge: 1000 * 60 * 60 // life time in milliseconds
+  }});
 var city = cityLookup.get('42.113.94.71');
 
 console.log(city);
